f9a35381028db51309d7e77b0f1603f2fe073025
Aic <73454440+SereneCai@users.noreply.github.com>
2021-05-12 16:47:03 +0800

    user schema added

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const flash = require('connect-flash');
+const passport = require('passport');
+const bodyParser = require('body-parser');
+
+
+const PORT = process.env.PORT || 4000;
+var MongoDBStore = require('connect-mongodb-session')(session);

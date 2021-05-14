a3816a85eafd4f78a89946d2ad12e1c2a24830db
Aic <73454440+SereneCai@users.noreply.github.com>
2021-05-14 12:11:01 +0800

    added db

--- app.js
+++ app.js
@@ -6,9 +6,20 @@ const flash = require('connect-flash');
 const passport = require('passport');
 const bodyParser = require('body-parser');
 const user = require('./model/user');
-
 const app = express();
+const dbURI = require('./config/keys').MongoURI;
+//register view engine
+//views is the default folder it look for
+// if other folder is used, put another app.set below, --> app.set('<mainfoldername>', '<foldername>');
+mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true}) //2nd parameter to stop deprecation warning
+    .then((result)=>app.listen(3000))
+    .catch((err) => console.log(err));
+
 app.set('view engine', 'ejs');
+
+//middleware & static file
+app.use(express.static('public'));
+app.use(express.urlencoded({extended:true})); // a middleware that allow drawing of input for data from form
 
 
 const PORT = process.env.PORT || 4000;
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MongoURI: 'mongodb+srv://tsk_1:Adaptivityproject2021@cluster0.jk37u.mongodb.net/IP-project?retryWrites=true&w=majority'
+}

8c9261213ce2980e03cb20d998e3ebf4854aec29
Isaac Chong <ichong1996.ic@gmail.com>
2021-05-20 10:58:36 +0800

    Changes

--- controller/userController.js
+++ controller/userController.js
@@ -1,64 +1,60 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const User = require('../model/user');
-const bcrypt = require ('bcryptjs');
+const User = require("../model/user");
+const bcrypt = require("bcryptjs");
 
+router.get("/login", (req, res) => res.render("login"));
 
-router.get('/login', (req,res) => res.render('login'));
-
-router.get('/register', (req,res) => res.render('register'));
+router.get("/register", (req, res) => res.render("register"));
 
 //register handle
-router.post('/register', (req,res)=>{
-    const {name, email, password} = req.body;
-    let errors =[];
-    //check required fields
-    if(!name || !email || !password){
-        errors.push({msg: 'Please fill in all fields'});
-    }
+router.post("/register", (req, res) => {
+  const { name, email, password } = req.body;
+  let errors = [];
+  //check required fields
+  if (!name || !email || !password) {
+    errors.push({ msg: "Please fill in all fields" });
+  }
 
-    //check pass length
-    if(password.length < 6){
-        errors.push({msg: 'Password have to be more than 6 letters'})
-    }
+  //check pass length
+  if (password.length < 6) {
+    errors.push({ msg: "Password have to be more than 6 letters" });
+  }
 
-    if(errors.length > 0){
-        res.render('register', {errors, name, email, password})
-    }
-    else{
-        User.findOne({email:email})
-            .then(user =>{
-                if(user){
-                    //user exist
-                    errors.push({msg: 'Already registered.'})
-                    res.render('register', {errors, name, email, password})
-                }
-                else{
-                    const newUser = new User({
-                        name,
-                        email,
-                        password
-                    })
-                    //hash password
-                    bcrypt.genSalt(10,(err, salt)=>
-                        bcrypt.hash(newUser.password, salt, (err,hash)=>{
-                            if(err){
-                                throw err;
-                            }
-                            //set password to hash
-                            newUser.password = hash;
-                            newUser.save()
-                                .then(user =>{
-                                    req.flash('success_msg', 'You are registered and can log in.');
-                                    res.redirect('/users/login');
-                                })
-                                .catch(err => console.log(err))
-                        }))
-                }
-            });
-
-    }
-
-})
+  if (errors.length > 0) {
+    res.render("register", { errors, name, email, password });
+  } else {
+    User.findOne({ email: email }).then((user) => {
+      if (user) {
+        //user exist
+        errors.push({ msg: "Already registered." });
+        res.render("register", { errors, name, email, password });
+      } else {
+        const newUser = new User({
+          name,
+          email,
+          password,
+        });
+        //hash password
+        bcrypt.genSalt(10, (err, salt) =>
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) {
+              throw err;
+            }
+            //set password to hash
+            newUser.password = hash;
+            newUser
+              .save()
+              .then((user) => {
+                req.flash("success_msg", "You are registered and can log in.");
+                res.redirect("/users/login");
+              })
+              .catch((err) => console.log(err));
+          })
+        );
+      }
+    });
+  }
+});
 
 module.exports = router;
--- routes/security.js
+++ routes/security.js
@@ -1,6 +1,10 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/', (req,res) => res.send('welcome'));
+router.get("/", (req, res) => res.render("login"));
+
+router.get("typeform", (req, res) => {
+  res.render("typeform");
+});
 
 module.exports = router;

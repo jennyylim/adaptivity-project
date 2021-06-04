a5c1eef15e59578c0520bef93869fdf5d17c3736
Jenny Lim <jen.miller1904@gmail.com>
2021-06-04 19:04:41 +0800

    Update product-manager in progress

--- routes/security.js
+++ routes/security.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-router.get('/', (req, res) => res.render("index"));
+router.get('/', (req, res) => res.render("product-manager"));
 
 router.get("/error", (req, res) => res.render("404"));
 
